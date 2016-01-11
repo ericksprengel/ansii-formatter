4541e1b3e63755c18711b973e316cf0ce12dab06
Erick M. Sprengel <erick.sprengel@lge.com>
2016-01-11 15:15:28 -0200

    remove unused function for ansii formatting.

--- extension/js/content.js
+++ extension/js/content.js
@@ -50,12 +50,12 @@
   ;
   
   // Open the port "jf" now, ready for when we need it
-    // console.time('established port') ;
+    console.time('established port') ;
     port = chrome.extension.connect({name: 'jf'}) ;
     
   // Add listener to receive response from BG when ready
     port.onMessage.addListener( function (msg) {
-      // console.log('Port msg received', msg[0], (""+msg[1]).substring(0,30)) ;
+      console.log('Port msg received', msg[0], (""+msg[1]).substring(0,30)) ;
       
       switch (msg[0]) {
         case 'NOT JSON' :
@@ -166,13 +166,6 @@
             // Put it in optionBar
               optionBar.appendChild(buttonPlain) ;
               optionBar.appendChild(buttonFormatted) ;
-
-          // Attach event handlers
-            document.addEventListener(
-              'click',
-              generalClick,
-              false // No need to propogate down
-            ) ;
           
           // Put option bar in DOM
             document.body.insertBefore(optionBar, pre) ;
@@ -257,140 +250,10 @@
         
           // Now, this script will just wait to receive anything back via another port message. The returned message will be something like "NOT JSON" or "IS JSON"
       }
-
-      document.addEventListener('keyup', function(e) {
-        if (e.keyCode === 37 && typeof buttonPlain !== 'undefined') {
-          buttonPlain.click();
-        }
-        else if (e.keyCode === 39 && typeof buttonFormatted !== 'undefined') {
-          buttonFormatted.click();
-        }
-      });
   }
   
   document.addEventListener("DOMContentLoaded", ready, false);
 
   var lastKvovIdGiven = 0 ;
-  function collapse(elements) {
-    // console.log('elements', elements) ;
-
-    var el, i, blockInner, count ;
-
-    for (i = elements.length - 1; i >= 0; i--) {
-      el = elements[i] ;
-      el.classList.add('collapsed') ;
-
-      // (CSS hides the contents and shows an ellipsis.)
-
-      // Add a count of the number of child properties/items (if not already done for this item)
-        if (!el.id) {
-          el.id = 'kvov' + (++lastKvovIdGiven) ;
-
-          // Find the blockInner
-            blockInner = el.firstElementChild ;
-            while ( blockInner && !blockInner.classList.contains('blockInner') ) {
-              blockInner = blockInner.nextElementSibling ;
-            }
-            if (!blockInner)
-              continue ;
-
-          // See how many children in the blockInner
-            count = blockInner.children.length ;
-
-          // Generate comment text eg "4 items"
-            var comment = count + (count===1 ? ' item' : ' items') ;
-          // Add CSS that targets it
-            jfStyleEl.insertAdjacentHTML(
-              'beforeend',
-              '\n#kvov'+lastKvovIdGiven+'.collapsed:after{color: #aaa; content:" // '+comment+'"}'
-            ) ;
-        }
-    }
-  }
-  function expand(elements) {
-    for (var i = elements.length - 1; i >= 0; i--)
-      elements[i].classList.remove('collapsed') ;
-  }
-
-  var mac = navigator.platform.indexOf('Mac') !== -1,
-      modKey ;
-  if (mac)
-    modKey = function (ev) {
-      return ev.metaKey ;
-    } ;
-  else
-    modKey = function (ev) {
-      return ev.ctrlKey ;
-    } ;
-
-  function generalClick(ev) {
-    // console.log('click', ev) ;
-
-    if (ev.which === 1) {
-      var elem = ev.target ;
-      
-      if (elem.className === 'e') {
-        // It's a click on an expander.
-
-        ev.preventDefault() ;
-
-        var parent = elem.parentNode,
-            div = jfContent,
-            prevBodyHeight = document.body.offsetHeight,
-            scrollTop = document.body.scrollTop,
-            parentSiblings
-        ;
-        
-        // Expand or collapse
-          if (parent.classList.contains('collapsed')) {
-            // EXPAND
-              if (modKey(ev))
-                expand(parent.parentNode.children) ;
-              else
-                expand([parent]) ;
-          }
-          else {
-            // COLLAPSE
-              if (modKey(ev))
-                collapse(parent.parentNode.children) ;
-              else
-                collapse([parent]) ;
-          }
-
-        // Restore scrollTop somehow
-          // Clear current extra margin, if any
-            div.style.marginBottom = 0 ;
-
-          // No need to worry if all content fits in viewport
-            if (document.body.offsetHeight < window.innerHeight) {
-              // console.log('document.body.offsetHeight < window.innerHeight; no need to adjust height') ;
-              return ;
-            }
-
-          // And no need to worry if scrollTop still the same
-            if (document.body.scrollTop === scrollTop) {
-              // console.log('document.body.scrollTop === scrollTop; no need to adjust height') ;
-              return ;
-            }
-
-          // console.log('Scrolltop HAS changed. document.body.scrollTop is now '+document.body.scrollTop+'; was '+scrollTop) ;
-          
-          // The body has got a bit shorter.
-          // We need to increase the body height by a bit (by increasing the bottom margin on the jfContent div). The amount to increase it is whatever is the difference between our previous scrollTop and our new one.
-          
-          // Work out how much more our target scrollTop is than this.
-            var difference = scrollTop - document.body.scrollTop  + 8 ; // it always loses 8px; don't know why
-
-          // Add this difference to the bottom margin
-            //var currentMarginBottom = parseInt(div.style.marginBottom) || 0 ;
-            div.style.marginBottom = difference + 'px' ;
-
-          // Now change the scrollTop back to what it was
-            document.body.scrollTop = scrollTop ;
-            
-        return ;
-      }
-    }
-  }
 
 })();
